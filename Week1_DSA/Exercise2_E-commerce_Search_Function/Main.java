e4d7089a0ebe6463a567a62e4c6aeeee25717bb2
Rishi Prakash <19rishi09@gmail.com>
2026-06-26 23:54:56 +0530

    Completed Week 1 DSA Exercise 2 - Search Algorithms

---
+++ Week1_DSA/Exercise2_E-commerce_Search_Function/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Product[] products = {
+
+                new Product(101, "Laptop", "Electronics"),
+                new Product(102, "Mobile", "Electronics"),
+                new Product(103, "Shoes", "Fashion"),
+                new Product(104, "Watch", "Accessories"),
+                new Product(105, "Bag", "Fashion")
+
+        };
+
+        Product result1 = SearchAlgorithms.linearSearch(products, 103);
+
+        if (result1 != null) {
+            System.out.println("Linear Search Found:");
+            System.out.println(result1.productName + " - " + result1.category);
+        } else {
+            System.out.println("Product Not Found");
+        }
+
+        Product result2 = SearchAlgorithms.binarySearch(products, 104);
+
+        if (result2 != null) {
+            System.out.println("\nBinary Search Found:");
+            System.out.println(result2.productName + " - " + result2.category);
+        } else {
+            System.out.println("Product Not Found");
+        }
+    }
+}
